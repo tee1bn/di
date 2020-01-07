1c8980424059c21d9dfb447123326069bbbd1710
Taiwo <tee01bn@gmail.com>
2020-01-07 23:47:34 +0100

    Update index.php

--- template/default/guest/index.php
+++ template/default/guest/index.php
@@ -94,29 +94,61 @@ include_once 'includes/header.php';?>
 </div>
 <!-- Counters end -->
 
-
-<!-- Testimonial 1 start -->
-<div class="testimonial-1"  style="background: black !important;">
+<div class="testimonial-1 overview-bgi">
     <div class="container">
         <div class="row">
             <div class="offset-lg-2 col-lg-8">
                 <div class="testimonial-inner">
                     <header class="testimonia-header">
-                        <h1><span></span> Participants Say</h1>
+                        <h1><span>Our</span> Testimonial</h1>
                     </header>
+                    <div id="carouselExampleIndicators7" class="carousel slide" data-ride="carousel">
+                        <div class="carousel-inner">
 
 
-                    
 
 
-                   
+
+                            <?php $i=1; foreach (Testimonials::approved()->get()->take(6) as $testimonial) :?>
+                                    <div class="carousel-item <?=($i==1)?'active' :'';?>" >
+                                        <div class="row">
+                                            <div class="col-xl-9 col-lg-9 col-md-9 col-sm-12 col-xs-12">
+                                                <p class="lead">
+                                                    <?=$testimonial->content;?>
+                                                </p>
+                                                <div class="author-name">
+                                                    <?=$testimonial->attester;?>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    
+
+                                <?php $i++; endforeach;?>
+
+
+
+
+
+
+                        </div>
+
+                        <a class="carousel-control-prev" href="#carouselExampleIndicators7" role="button" data-slide="prev">
+                            <span class="slider-mover-left" aria-hidden="true">
+                                <i class="fa fa-angle-left"></i>
+                            </span>
+                        </a>
+                        <a class="carousel-control-next" href="#carouselExampleIndicators7" role="button" data-slide="next">
+                            <span class="slider-mover-right" aria-hidden="true">
+                                <i class="fa fa-angle-right"></i>
+                            </span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
 <!-- Testimonial 1 end -->
-
-
 
 <?php include_once 'includes/footer.php';?>
